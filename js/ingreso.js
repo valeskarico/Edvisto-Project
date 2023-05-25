b259eeea82597f53ed1e9c6870fece2e9ef8dca5
valeskarico <124623365+valeskarico@users.noreply.github.com>
2023-05-24 22:19:56 -0300

    update ingreso.js
    mensajes de alerta

--- js/ingreso.js
+++ js/ingreso.js
@@ -12,97 +12,52 @@ function redirectAfterLogin(rol) {
   return window.location.href = 'pages/estudiante.html';
 }
 
-function login(e) {
-  e.preventDefault();
+function validarEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+function validarFormulario(event) {
+  event.preventDefault(); // Evitar que el formulario se envíe por defecto
+
+  const emailInput = document.getElementById('email');
+  const email = emailInput.value;
+
+  if (!validarEmail(email)) {
+    Swal.fire({
+      text: 'Por favor, introduce un correo electrónico válido',
+      confirmButtonColor: '#354760',
+    })
+    return;
+  }
+
+  const profesorRadio = document.querySelector('.profesor input[type="radio"]');
+  const estudianteRadio = document.querySelector('.estudiante input[type="radio"]');
+
+  if (!profesorRadio.checked && !estudianteRadio.checked) {
+    // Ningún radio está marcado, mostrar mensaje de error o realizar alguna otra acción
+    Swal.fire({
+      text: 'Por favor, selecciona un rol',
+      confirmButtonColor: '#354760',
+    });
+    return;
+  }
+
   axios.post(
     `${baseURl}/auth/login`,
-    getFormData(e.target),
+    getFormData(event.target),
   ).then((response) => {
     localStorage.setItem('user', JSON.stringify(response.data));
     redirectAfterLogin(response.data.rol);
   }).catch(() => {
     Swal.fire({
-      title: 'Usuario no encontrado',
-      text: 'Asegurese que el email se encuentra registrado y que el rol seleccionado es el correcto',
+      text: 'Asegúrese de que el email se encuentre registrado y que el rol seleccionado es el correcto',
       confirmButtonColor: '#354760',
-    })
+    });
   });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   const loginForm = document.getElementById('login');
-  loginForm.addEventListener('submit', login);
+  loginForm.addEventListener('submit', validarFormulario);
 });
-/*
-function validarAcceso() {
-  var email = document.getElementById("email").value;
-  var rol = document.querySelector('input[name="rol"]:checked').value;
-
-  if (email === "profesor@email.com" && rol === "teacher") {
-    localStorage.setItem("emailProfesor", email);
-    window.location.href = "./pages/profesor.html";
-  } else if (email === "estudiante@email.com" && rol === "student") {
-    localStorage.setItem("emailEstudiante", email);
-    window.location.href = "./pages/estudiante.html";
-  }
-  return false;
-}
-
-const emailInput = document.getElementById('email');
-const rolInputs = document.getElementsByName('rol');
-
-formulario.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const email = emailInput.value.trim();
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-
-  if (!emailRegex.test(email)) {
-
-    //alert('El correo electrónico ingresado no es válido');
-    Swal.fire({
-      imageUrl: 'img/error-email.png',
-      confirmButtonColor: '#354760',
-    })
-    return;
-
-  } else if (email === 'profesor@email.com') {
-
-    if (rolInputs[0].checked) {
-
-    } else if (rolInputs[1].checked) {
-      Swal.fire({
-        title: 'El correo electrónico ingresado no corresponde al rol',
-        confirmButtonColor: '#354760',
-      })
-    } else {
-      Swal.fire({
-        title: 'Seleccione un rol de usuario',
-        confirmButtonColor: '#354760',
-      })
-    }
-
-  } else if (email === 'estudiante@email.com') {
-    if (rolInputs[1].checked) {
-
-    } else if (rolInputs[0].checked) {
-      Swal.fire({
-        title: 'El correo electrónico ingresado no corresponde al rol',
-        confirmButtonColor: '#354760',
-      })
-    } else {   
-      Swal.fire({
-        title: 'Seleccione un rol de usuario',
-        confirmButtonColor: '#354760',
-      })
-    }
-  } else {
-    Swal.fire({
-      imageUrl: 'img/no-registrado.png',
-      confirmButtonColor: '#354760',
-    })
-  }
-});
-*/
